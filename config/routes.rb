adc05fd7bc80dd17f40432d9fb6bfe5917acc4c6
Athena Saldanha <asaldanha@chicagobooth.edu>
2023-04-13 14:33:03 +0000

    Aries, Taurus, Leo

--- config/routes.rb
+++ config/routes.rb
@@ -20,9 +20,8 @@ Rails.application.routes.draw do
 
   get("/zodiacs/aries", { :controller =>  "fire", :action =>  "ram" })
   get("/zodiacs/leo", { :controller => "fire", :action => "lion" })
-  # get("/zodiacs/sagittarius" { :controller => "fire", :action => "archer" })
-  
-  # get("/zodiacs/taurus", { :controller, "earth", :action, "bull" })
+  get("/zodiacs/sagittarius", { :controller => "fire", :action => "archer" })
+  get("/zodiacs/taurus", { :controller => "earth", :action => "bull" })
   # get("/zodiacs/virgo", { :controller => "earth", :action => "maiden" })
   # get("/zodiacs/capricorn", { :controller => "earth", :action => "goat" })
   
